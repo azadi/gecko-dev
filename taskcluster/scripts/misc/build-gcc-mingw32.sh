40d57639d9a01017b71d6ed0ee2159fbc8f694a9
Tom Ritter <tom@mozilla.com>
2018-05-31 12:10:50 -0500

    Bug 1434316 Add 64 bit MinGW gcc toolchains
    MozReview-Commit-ID: CBUE6i8ghpx

--- taskcluster/scripts/misc/build-gcc-mingw32.sh
+++ taskcluster/scripts/misc/build-gcc-mingw32.sh
@@ -14,10 +14,11 @@ data_dir=$HOME_DIR/src/build/unix/build-gcc
 . $data_dir/build-gcc.sh
 
 gcc_version=6.4.0
+gcc_target=$1
 gcc_ext=xz
 binutils_version=2.27
 binutils_ext=bz2
-binutils_configure_flags="--target=i686-w64-mingw32"
+binutils_configure_flags="--target=$gcc_target"
 mingw_version=bcf1f29d6dc80b6025b416bef104d2314fa9be57
 
 # GPG keys used to sign GCC (collected from 5.1.0, 5.4.0, 6.4.0)
